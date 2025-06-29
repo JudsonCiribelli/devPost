d92244859b8436fd45b1c763bcaf5889779c12d6
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-06-29 19:49:25 -0300

    feat: add Input component

---
+++ src/components/Input-Component/inputComponent.tsx
@@ -0,0 +1,35 @@
+import type { RegisterOptions, UseFormRegister } from "react-hook-form";
+
+interface InputComponentProps {
+  name: string;
+  type: string;
+  placeholder: string;
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions;
+}
+
+const InputComponent = ({
+  name,
+  type,
+  placeholder,
+  register,
+  error,
+  rules,
+}: InputComponentProps) => {
+  return (
+    <div>
+      <input
+        className="w-full bg-white text-black p-2 rounded-lg outline-none"
+        {...register(name, rules)}
+        id={name}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+      />
+      {error && <p className="text-red-600 mb-2 font-semibold">{error}</p>}
+    </div>
+  );
+};
+
+export default InputComponent;
