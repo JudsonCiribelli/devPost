e2547225e46d87ec0a1e3afff93e787dda5e16ea
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-06-30 13:42:21 -0300

    feat: add user profile routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/Home/home";
 import LoginPage from "./pages/Login/login";
 import PostsPage from "./pages/Posts/posts";
 import RegisterPage from "./pages/Register/register";
+import UserProfilePage from "./pages/User/user";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         element: <RegisterPage />,
         path: "/register",
+      },
+      {
+        element: <UserProfilePage />,
+        path: "/user/:id",
       },
     ],
   },
