1e45ee9d38973601ddce1f725c79f9f9897eadf1
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-06-30 13:43:07 -0300

    refactor: redirect user to your profile when he logs in

--- src/components/Header-Component/headerComponent.tsx
+++ src/components/Header-Component/headerComponent.tsx
@@ -1,7 +1,19 @@
+import { signOut } from "firebase/auth";
+import { useContext } from "react";
 import { FaUser } from "react-icons/fa";
+import { IoLogOut } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
+import { AppContext } from "../../context/AppContext";
+import { auth } from "../../services/firebaseConection";
+
 const HeaderComponent = () => {
+  const { signed, user } = useContext(AppContext);
+
+  const handleSignOutUser = async () => {
+    await signOut(auth);
+  };
+
   return (
     <header className="w-full flex items-center justify-around p-4 bg-slate-200">
       <Link to="/">
@@ -11,9 +23,14 @@ const HeaderComponent = () => {
           </h1>
         </div>
       </Link>
-      <button className="cursor-pointer bg-amber-50 p-2 rounded-full">
-        <FaUser size={26} />
-      </button>
+      <Link to={`/user/${user?.uid}`}>
+        <button
+          className="cursor-pointer bg-amber-50 p-2 rounded-full"
+          onClick={handleSignOutUser}
+        >
+          {signed ? <IoLogOut size={26} /> : <FaUser size={26} />}
+        </button>
+      </Link>
     </header>
   );
 };
