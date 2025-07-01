c6a85ca17786705de2a2306f54230cdd1bac536a
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-07-01 20:40:24 -0300

    feat: add initial setup on user page and function to add a post

---
+++ src/pages/User/user.tsx
@@ -0,0 +1,103 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { addDoc, collection } from "firebase/firestore";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { Navigate } from "react-router-dom";
+import z from "zod";
+
+import InputComponent from "../../components/Input-Component/inputComponent";
+import { AppContext } from "../../context/AppContext";
+import { db } from "../../services/firebaseConection";
+
+const schema = z.object({
+  title: z.string().nonempty("Preencha todos os campos!"),
+  author: z.string().nonempty("Preencha todos os campos!"),
+  description: z.string().nonempty("Preencha todos os campos!"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const UserProfilePage = () => {
+  const { signed } = useContext(AppContext);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  if (!signed) {
+    return <Navigate to="/login" />;
+  }
+
+  const handleAddPost = (data: FormData) => {
+    addDoc(collection(db, "posts"), {
+      Author: data.author,
+      Description: data.description,
+      Title: data.title,
+      created: new Date(),
+    })
+      .then(() => {
+        reset();
+        toast.success("Post adicionado com sucesso!");
+      })
+      .catch((error) => {
+        toast.error("Ocorreu um erro na sua postagem!");
+        console.log(error);
+      });
+  };
+
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <section className="w-2xl flex flex-col h-105 max-h-screen  bg-slate-500 rounded-xl p-4 m-3 gap-2">
+        <h1 className="text-black font-medium text-2xl text-center">
+          Postagem
+        </h1>
+        <form className="w-full p-2" onSubmit={handleSubmit(handleAddPost)}>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl  text-black ">Título</label>
+            <InputComponent
+              register={register}
+              placeholder="Digite o título do post"
+              name="title"
+              type="text"
+              error={errors.title?.message}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl  text-black ">Autor</label>
+            <InputComponent
+              register={register}
+              placeholder="Digite seu nome"
+              name="author"
+              type="text"
+              error={errors.author?.message}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl  text-black ">Descrição</label>
+            <textarea
+              className="border-1 rounded-lg bg-white text-black w-full h-24 px-2 outline-none mb-2"
+              {...register("description")}
+              name="description"
+              id="description"
+              placeholder="Digite seu comentário"
+            />
+          </div>
+          <button
+            className="w-full items-center justify-center bg-black text-white text-lg p-1.5 rounded-lg font-medium"
+            type="submit"
+          >
+            Postar
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default UserProfilePage;
