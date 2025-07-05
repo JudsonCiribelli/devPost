f5bc40db79640fe3e71492a429851c136ddcc24c
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-07-04 22:07:27 -0300

    feat: add initial setup on user page

--- src/pages/User/user.tsx
+++ src/pages/User/user.tsx
@@ -7,6 +7,7 @@ import { Navigate } from "react-router-dom";
 import z from "zod";
 
 import InputComponent from "../../components/Input-Component/inputComponent";
+import NavComponent from "../../components/NavBar-Component/navComponent";
 import { AppContext } from "../../context/AppContext";
 import { db } from "../../services/firebaseConection";
 
@@ -19,7 +20,7 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 const UserProfilePage = () => {
-  const { signed } = useContext(AppContext);
+  const { signed, user } = useContext(AppContext);
   const {
     register,
     handleSubmit,
@@ -52,49 +53,60 @@ const UserProfilePage = () => {
   };
 
   return (
-    <main className="w-full h-screen flex items-center justify-center">
-      <section className="w-2xl flex flex-col h-105 max-h-screen  bg-slate-500 rounded-xl p-4 m-3 gap-2">
-        <h1 className="text-black font-medium text-2xl text-center">
-          Postagem
-        </h1>
-        <form className="w-full p-2" onSubmit={handleSubmit(handleAddPost)}>
+    <main className="w-full h-screen flex flex-col items-center justify-center">
+      <NavComponent />
+      <section className="flex items-center justify-between m-2 p-2">
+        <div className="border-1 rounded-xl w-xs h-105 bg-slate-500  ">
+          <h1 className="text-black font-medium text-xl text-center p-4">
+            Dados do usuario
+          </h1>
           <div className="flex flex-col gap-1">
-            <label className="text-xl  text-black ">Título</label>
-            <InputComponent
-              register={register}
-              placeholder="Digite o título do post"
-              name="title"
-              type="text"
-              error={errors.title?.message}
-            />
+            <p>Nome: {user?.name}</p>
           </div>
-          <div className="flex flex-col gap-1">
-            <label className="text-xl  text-black ">Autor</label>
-            <InputComponent
-              register={register}
-              placeholder="Digite seu nome"
-              name="author"
-              type="text"
-              error={errors.author?.message}
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label className="text-xl  text-black ">Descrição</label>
-            <textarea
-              className="border-1 rounded-lg bg-white text-black w-full h-24 px-2 outline-none mb-2"
-              {...register("description")}
-              name="description"
-              id="description"
-              placeholder="Digite seu comentário"
-            />
-          </div>
-          <button
-            className="w-full items-center justify-center bg-black text-white text-lg p-1.5 rounded-lg font-medium"
-            type="submit"
-          >
-            Postar
-          </button>
-        </form>
+        </div>
+        <div className="flex flex-col w-xl max-w-2xl h-105 max-h-screen bg-slate-500 rounded-xl p-4 m-3 gap-2">
+          <h1 className="text-black font-medium text-xl text-center">
+            Postagem
+          </h1>
+          <form className="w-full p-2" onSubmit={handleSubmit(handleAddPost)}>
+            <div className="flex flex-col gap-1">
+              <label className="text-xl  text-black ">Título</label>
+              <InputComponent
+                register={register}
+                placeholder="Digite o título do post"
+                name="title"
+                type="text"
+                error={errors.title?.message}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label className="text-xl  text-black ">Autor</label>
+              <InputComponent
+                register={register}
+                placeholder="Digite seu nome"
+                name="author"
+                type="text"
+                error={errors.author?.message}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label className="text-xl  text-black ">Descrição</label>
+              <textarea
+                className="border-1 rounded-lg bg-white text-black w-full h-24 px-2 outline-none mb-2"
+                {...register("description")}
+                name="description"
+                id="description"
+                placeholder="Digite seu comentário"
+              />
+            </div>
+            <button
+              className="w-full items-center justify-center bg-black text-white text-lg p-1.5 rounded-lg font-medium"
+              type="submit"
+            >
+              Postar
+            </button>
+          </form>
+        </div>
       </section>
     </main>
   );
